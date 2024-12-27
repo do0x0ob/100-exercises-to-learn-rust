76dd01162446f9e75e8395488b232945608fe8ba
do0x0ob <do0x0ob@protonmail.com>
2024-12-27 10:52:24 +0800

    Progress Update 20241227

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -1,6 +1,28 @@
 // TODO: implement a so-called "Drop bomb": a type that panics when dropped
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
+struct DropBomb {
+    defused: bool,
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        Self { defused: true }
+    }
+
+    fn defuse(&mut self) {
+        self.defused = false;
+    }
+
+}
+
+impl Drop for DropBomb {
+    fn drop (&mut self) {
+        if self.defused {
+            panic!("Boom!")
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,94 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl SaturatingU16 {
+    fn saturating_add(a: u16, b: u16) -> u16 {
+        a.saturating_add(b)
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self {
+            value: value as u16,
+        }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        Self { value: *value }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        Self {
+            value: *value as u16,
+        }
+    }
+}
+
+impl std::ops::Add for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            value: Self::saturating_add(self.value, other.value),
+        }
+    }
+}
+
+impl std::ops::Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: u16) -> Self::Output {
+        Self {
+            value: Self::saturating_add(self.value, other),
+        }
+    }
+}
+
+impl std::ops::Add<&u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: &u16) -> Self::Output {
+        Self {
+            value: Self::saturating_add(self.value, *other),
+        }
+    }
+}
+
+impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: &SaturatingU16) -> Self::Output {
+        Self {
+            value: Self::saturating_add(self.value, other.value),
+        }
+    }
+}
+
+impl From<SaturatingU16> for u16 {
+    fn from(value: SaturatingU16) -> Self {
+        value.value
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
